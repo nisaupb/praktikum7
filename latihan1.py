732cfb53c955458c2cf76633769d921435a934d2
ANNISA FITRIANI <annisafitri112501@gmail.com>
2022-11-20 18:46:03 +0700

    simpan

---
+++ latihan1.py
@@ -0,0 +1,8 @@
+a =input('masukan bilangan ke-1 : ')
+b =input('masukan bilangan ke-2 : ')
+
+if a > b:
+    print('A bilangan terbesar')
+    
+elif b > a :
+    print('B bilangan terbesar')
---
+++ latihan4.py
@@ -0,0 +1,9 @@
+import random
+
+
+jumlah = int(input("Masukan Jumlah Nilai :"))
+for i in range(jumlah):
+    i=random.uniform(0.0,0.5)
+    print("data ke:", i)
+    
+    print("SELESAI")
